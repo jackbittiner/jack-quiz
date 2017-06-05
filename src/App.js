111fc35309972fe8a30f428104d47ba1cd10ed2e
Jack Bittiner <jackbittiner@hotmail.com>
2017-06-05 21:11:35 +0100

    questions written

--- src/App.js
+++ src/App.js
@@ -2,8 +2,28 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Question from './components/Question';
+import quizQuestions from './api/quizQuestions';
 
 class App extends Component {
+
+  constructor(props) {
+  super(props);
+
+  this.state = {
+   counter: 0,
+   questionId: 1,
+   question: '',
+   answerOptions: [],
+   answer: '',
+   answersCount: {
+     yes: 0,
+     definitely: 0,
+     withoutDoubt: 0
+   },
+   result: ''
+  };
+}
+
   render() {
     return (
       <div className="App">
---
+++ src/questions/quizQuestions.js
@@ -0,0 +1,89 @@
+var quizQuestions = [
+  {
+      question: "How many times do you eat crisps a week?",
+      answers: [
+          {
+              type: "yes",
+              content: "NEVER, THEY'RE THE DEVILS'S PO-POs"
+          },
+          {
+              type: "definitely",
+              content: "Maybe like once or twice in the meal deal!"
+          },
+          {
+              type: "withoutDoubt",
+              content: "I eat all the crisps!"
+          }
+      ]
+  },
+  {
+      question: "Are you a wreck'ead?",
+      answers: [
+        {
+            type: "yes",
+            content: "Only at festivals"
+        },
+        {
+            type: "definitely",
+            content: "Only on weekends"
+        },
+        {
+            type: "withoutDoubt",
+            content: "Only at aquariums, tapas restaurants and the Fonz's flat..."
+        }
+      ]
+  },
+  {
+      question: "Does Jack look like adrian brody?",
+      answers: [
+        {
+            type: "yes",
+            content: "Yes!"
+        },
+        {
+            type: "definitely",
+            content: "No!"
+        },
+        {
+            type: "withoutDoubt",
+            content: "Probably about a 6 out of 10 maybe"
+        }
+      ]
+  },
+  {
+      question: "If you were a fritata filling, what fritata filling would you be?",
+      answers: [
+        {
+            type: "yes",
+            content: "Spinach, yum yum"
+        },
+        {
+            type: "definitely",
+            content: "Chorizo, yum yum"
+        },
+        {
+            type: "withoutDoubt",
+            content: "Chorizo and Spinach, yum yum"
+        }
+      ]
+  },
+  {
+      question: "Where are mum and dad sleeping?",
+      answers: [
+        {
+            type: "yes",
+            content: "Will's Room"
+        },
+        {
+            type: "definitely",
+            content: "Will's Brother's Room"
+        },
+        {
+            type: "withoutDoubt",
+            content: "Mum and Dad's Room"
+        }
+      ]
+  }
+];
+
+export default quizQuestions;
